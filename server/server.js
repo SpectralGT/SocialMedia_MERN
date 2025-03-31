0b644508072c955dbc4b3676fcd0efa78fe6bc74
SpectralGT <atharvsingh1396@gmail.com>
2025-03-31 17:19:37 +0530

    fixed errors

--- server/server.js
+++ server/server.js
@@ -3,4 +3,12 @@ import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cor from 'cors';
 
-const express = require('express')
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(3000, () => {
+  console.log(`Example app listening on port 3000`)
+})
