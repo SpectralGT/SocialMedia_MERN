2ce8ee67d2355f098a3d3e3caa9b97f7320c68d0
SpectralGT <atharvsingh1396@gmail.com>
2025-04-02 11:45:10 +0530

    added body parser middleware

--- server/server.js
+++ server/server.js
@@ -1,14 +1,25 @@
+import { config } from "dotenv";
 import express from "express";
-import bodyParser from "body-parser";
 import mongoose from "mongoose";
-import cor from 'cors';
+import bodyParser from "body-parser";
+import cors from 'cors';
 
 const app = express();
+// to config the env file
+config();
+// app.use() is used to mount middleware functions that handle incoming 
+// HTTP requests before they reach your routes and controllers
+app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+// if incoming request contains image, we set the size limit
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
 
-app.listen(3000, () => {
-  console.log(`Example app listening on port 3000`)
-})
+const MONGODB_URL = process.env.MONGODB_URL;
+const PORT = process.env.PORT;
+
+mongoose.connect(MONGODB_URL)
+  .then(() =>
+    app.listen(PORT, () => console.log(`Server open in port: ${PORT}`))
+  ).catch((error) => console.log(error));
